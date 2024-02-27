4b423ae42d30ad796ebaa1d46bdca0464496faa1
LucasScarone <lucasscarone01@gmail.com>
2024-02-27 09:30:30 -0300

    sprint2

--- index.js
+++ index.js
@@ -45,27 +45,121 @@ function createCard( movies ){
     const p2 = document.createElement( 'p' )
     p2.textContent = movies.overview
     p2.classList.add("line-clamp-3")
+     
+    const a= document.createElement('a')
+    a.setAttribute('class', "font-semibold text-[#6D38E0]")
+    a.innerHTML = "See more..."
+    a.setAttribute('href', `./detalles.html?id=${movies.id}`)
+
     /* element.append me permite agregar varios a la vez */
-    article.append( img, h3, h4, p, p2 )
+    article.append( img, h3, h4, p, p2, a)
 
     return article
 } 
 
 
 function renderCards( movies, element, fn ){
-    /* Recorro la lista de personajes */
 
     const fragment = document.createDocumentFragment()
+    if (movies.length == 0) {
+        return element.innerHTML = `<h2>There are no movies with these filters</h2>`
+    }
     for (const iterator of movies) {
         /* Ejecuto la funcion que crear el nodo */
         const newArticle = fn( iterator ) 
         /* Agrego el nodo al elemento */
         fragment.appendChild( newArticle )
     }
+    element.innerHTML = "" 
     element.appendChild( fragment )
 
 }
 
 renderCards( peliculas, div, createCard )
+
+const div2=document.getElementById('contentselect')
+
+// barra de busqueda
+// 1 crear la etiquet 
+// 2poner el imput 
+// 3trear el DOM
+const inputBusqueda = document.getElementById('inputBusqueda')
+// 4indentificar el evento 
+
+// 5funcion para filtrar las peliculas por su nombre 
+// 6agregarñlos a un array nuevo 
+// 7enlazr la funcion con el evento 
+// 8indicarle al evento lo que quuiero que haga 
+// 9imprimir por pantallala nueva lista cda vez que se busque 
+// 10 despues tengo qe hacer que si no encuentra niguno me de un aviso
+
+const genres= peliculas.map(pelicula => pelicula.genres).flat()
+
+const genresSinRepetidos= []
+genres.forEach ( genres =>{
+    if(!genresSinRepetidos.includes(genres)){
+        genresSinRepetidos.push(genres)
+    }
+})
+
+// console.log(genresSinRepetidos)
+
+function creareOption(params) {
+    const option =document.createElement('option')
+    option.setAttribute('value', `${params}`)
+    option.textContent = params
+    return option
+    
+}
+function crearselect( array, element, fn ){
+    const select = document.createElement( 'select' )
+    select.setAttribute('class', 'bg-[#D2CCFF] h-9 w-full border border-black rounded')
+    select.setAttribute('id', 'select')
+    const option =document.createElement('option')
+    option.setAttribute('value', 'all')
+    option.textContent = 'All'
+    select.appendChild(option)
+    for (const iterator of array) {
+        /* Ejecuto la funcion que crear el nodo */
+        const newOption = fn( iterator ) 
+        /* Agrego el nodo al elemento */
+        select.appendChild( newOption )
+    }
+    element.appendChild( select )
+    
+}
+
+crearselect(genresSinRepetidos, div2, creareOption)
+
+inputBusqueda.addEventListener('input',()=>{
+    const peliFiltradoXTiTulo= filtrarpeliculasporTitulo(peliculas, inputBusqueda.value)
+    const filtarPeliXgenres = filtarPeliculasXgenres (peliFiltradoXTiTulo, select)
+    renderCards(filtarPeliXgenres, div, createCard );
+})
+
+const select = document.getElementById('select')
+
+
+function filtrarpeliculasporTitulo(ListadePeliculas, title) {
+   return ListadePeliculas.filter(pelicula => pelicula.title.toLowerCase().startsWith(title.toLowerCase()))
+} 
+
+
+select.addEventListener( 'change', ()=>{
+    const peliculasfiltradasXgenres= filtarPeliculasXgenres(peliculas, select)
+    const peliFiltradoXTiTulo= filtrarpeliculasporTitulo(peliculasfiltradasXgenres, inputBusqueda.value)
+
+    renderCards(peliFiltradoXTiTulo, div, createCard)
+})
+
+function filtarPeliculasXgenres(ListadePeliculas, select){
+    console.log(select.value)
+    if (select.value === "all") {
+        return ListadePeliculas
+    }
+    return ListadePeliculas.filter(pelicula => pelicula.genres.includes(select.value))
+}
+
+
 
 
